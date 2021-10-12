6e291aeb7abe0dc7241ffcea9452b54850fe26d2
wawanneutron <wawanneutron1331@gmail.com>
2021-10-13 00:36:10 +0700

    hhtps

--- src/api/Api.js
+++ src/api/Api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const Api = axios.create({
   // baseURL: "http://127.0.0.1:8000/api",
-  baseURL: "http://ecommerce.timmixcode.my.id/api",
+  baseURL: "https://ecommerce.timmixcode.my.id/api",
 });
 
 export default Api;
