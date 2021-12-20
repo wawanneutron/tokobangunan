073f9e8ddb0c60c385632066610723d50987ba0a
wawanneutron <wawanneutron1331@gmail.com>
2021-12-20 19:47:35 +0700

    contact and about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,21 @@
 <template>
-  <div style="margin-top: 100px">
-    <h1>Hello World</h1>
-    <h5>This is About</h5>
+  <div class="container">
+    <div class="row justify-content-center text-center">
+      <div class="col-md-8">
+        <h1>Tantang Kami</h1>
+        <div class="mt-5">
+          <p style="line-height: 30px">
+            PT Chakra Gahana Gemilang adalah suatu bentuk usaha di bidang
+            pendistribusian dengan model penjualan dan pembelian bahan-bahan 2
+            konstruksi atau pembangunan. Ada beberapa jenis bahan bangunan yang
+            tersedia pada PT Chakra Gahana Gemilang seperti : besi beton, kawat,
+            wiremesh, spandek, bondek, paku dan lainnya. Kegiatan yang dilakukan
+            yaitu menyalurkan barang dari beberapa supplier ke beberapa toko
+            besi atau toko bangunan yang ada di wilayah kab. Tangerang, Depok,
+            Bogor, Bekasi, Serang, RangkasBitung, dan Pandeglang.
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,6 +1,42 @@
 <template>
-  <div style="margin-top: 100px">
-    <h1>Hello World</h1>
-    <h5>This is Contact</h5>
+  <div class="container contact">
+    <div class="row justify-content-center text-center">
+      <div class="col-md-8">
+        <h1>Hubungi kami melalui kontak</h1>
+        <div class="mt-5">
+          <div class="row">
+            <div class="col-md-6">
+              <div class="card border-0">
+                <div class="card-body">
+                  <a
+                    href="mailto:help@chackra.com"
+                    class="btn-icon"
+                    target="_blank"
+                    ><i
+                      class="far fa-envelope fa-3x"
+                      style="color: #eb0000"
+                    ></i>
+                    <div class="title mt-2">help@chackra.com</div>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="card border-0">
+                <div class="card-body">
+                  <a href="tel:0215546585" class="btn-icon"
+                    ><i
+                      class="fas fa-phone-alt fa-3x"
+                      style="color: #eb0000"
+                    ></i>
+                    <div class="title mt-2">0215546585</div>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
